e62663f58a6436bd1f8ecec2f581e3b73d97a1ca
Kit Westneat <kit.westneat@gmail.com>
2018-04-19 17:13:18 -0400

    add logging for test purposes and basic mocha tests

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,5 +1,6 @@
 class Item {
   constructor(name, sellIn, quality){
+    console.log('Item constructor', name, sellIn, quality);
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -8,28 +9,34 @@ class Item {
 
 class Shop {
   constructor(items=[]){
+    console.log('Shop constructor', items);
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+    console.log('updateQuality', this.items[i].name);
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
+            console.log('updateQuality', 'quality - 1');
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
+          console.log('updateQuality', 'quality + 1');
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
+                console.log('updateQuality', 'quality + 1');
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
+                console.log('updateQuality', 'quality + 1');
               }
             }
           }
@@ -37,6 +44,7 @@ class Shop {
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
+        console.log('updateQuality', 'sellin + 1');
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
@@ -44,19 +52,23 @@ class Shop {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
+                console.log('updateQuality', 'quality - 1');
               }
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            console.log('updateQuality', 'quality = 0');
           }
         } else {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
+            console.log('updateQuality', 'quality + 1');
           }
         }
       }
     }
 
+    console.log('updateQuality', 'returning', JSON.stringify(this.items));
     return this.items;
   }
 }
--- js/test/gilded_rose_spec.js
+++ js/test/gilded_rose_spec.js
@@ -1,10 +1,64 @@
 let expect = require('chai').expect;
 let { Item, Shop } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+const BRIE = 'Aged Brie', PASS = 'Backstage passes to a TAFKAL80ETC concert', SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
+describe('Gilded Rose', function() {
+  describe('foo', function() {
+    it('create item', function() {
+      const item = new Item('foo', 0, 0);
+
+      expect(item.name).to.equal('foo');
+    });
+    it('basic test', function() {
+      const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0]).to.deep.equal({
+        name: 'foo',
+        sellIn: -1,
+        quality: 0,
+      });
+    });
+  });
+  describe(BRIE, function() {
+    it('basic test', function() {
+      const gildedRose = new Shop([ new Item(BRIE, 0, 0) ]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0]).to.deep.equal({
+        name: BRIE,
+        sellIn: -1,
+        quality: 2,
+      });
+    });
+  });
+  describe(PASS, function() {
+    it('basic test', function() {
+      const gildedRose = new Shop([ new Item(PASS, 0, 0) ]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0]).to.deep.equal({
+        name: PASS,
+        sellIn: -1,
+        quality: 0,
+      });
+    });
+  });
+  describe(SULFURAS, function() {
+    it('basic test', function() {
+      const gildedRose = new Shop([ new Item(SULFURAS, 0, 0) ]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0]).to.deep.equal({
+        name: SULFURAS,
+        sellIn: 0,
+        quality: 0,
+      });
+    });
   });
 });
