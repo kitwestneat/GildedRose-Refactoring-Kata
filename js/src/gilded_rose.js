267474fab38d5057cb4f5186bcbc6a578172fdd8
Kit Westneat <kit.westneat@gmail.com>
2018-04-19 17:48:07 -0400

    first attempt at refactor

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -9,6 +9,36 @@ class Item {
   }
 }
 
+function updateItemQuality(item) {
+  if (item.name == BRIE) {
+    item.quality++;
+  } else if (item.name == PASS) {
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    } else if (item.sellIn < SHORT_SELL) {
+      item.quality += 3;
+    } else if (item.sellIn < LONG_SELL) {
+      item.quality += 2;
+    } else {
+      item.quality++;
+    }
+  } else if (item.passedSellby()) {
+    item.quality -= 2;
+  } else {
+    item.quality--;
+  }
+
+  if (item.quality < 0) {
+    item.quality = 0;
+  } else if (item.quality > 50) {
+    item.quality = 50;
+  }
+}
+
+function updateItemSellIn(item) {
+  item.sellIn--;
+}
+
 class Shop {
   constructor(items=[]){
     console.log('Shop constructor', items);
@@ -16,61 +46,14 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-    console.log('updateQuality', this.items[i].name);
-      if (this.items[i].name != BRIE && this.items[i].name != PASS) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS) {
-            this.items[i].quality = this.items[i].quality - 1;
-            console.log('updateQuality', 'quality - 1');
-          }
-        }
-      } else {
-        if (this.items[i].quality < HIGH_QUALITY) {
-          this.items[i].quality = this.items[i].quality + 1;
-          console.log('updateQuality', 'quality + 1');
-          if (this.items[i].name == PASS) {
-            if (this.items[i].sellIn < LONG_SELL) {
-              if (this.items[i].quality < HIGH_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log('updateQuality', 'quality + 1');
-              }
-            }
-            if (this.items[i].sellIn < SHORT_SELL) {
-              if (this.items[i].quality < HIGH_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log('updateQuality', 'quality + 1');
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        console.log('updateQuality', 'sellin - 1');
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != BRIE) {
-          if (this.items[i].name != PASS) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
-                this.items[i].quality = this.items[i].quality - 1;
-                console.log('updateQuality', 'quality - 1');
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-            console.log('updateQuality', 'quality = 0');
-          }
-        } else {
-          if (this.items[i].quality < HIGH_QUALITY) {
-            this.items[i].quality = this.items[i].quality + 1;
-            console.log('updateQuality', 'quality + 1');
-          }
-        }
-      }
+      // legendary items don't need to change sellin or quality
+      if (this.items[i].name == SULFURAS)
+        continue;
+
+      updateItemSellIn(this.items[i]);
+      updateItemQuality(this.items[i]);
     }
 
-    console.log('updateQuality', 'returning', JSON.stringify(this.items));
     return this.items;
   }
 }
