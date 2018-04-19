deb9cfbda1179266515b10c4d8509cc70cebac84
Kit Westneat <kit.westneat@gmail.com>
2018-04-19 16:53:25 -0400

    switch to chai

--- js/test/gilded_rose_spec.js
+++ js/test/gilded_rose_spec.js
@@ -1,9 +1,10 @@
+let expect = require('chai').expect;
 let { Item, Shop } = require('../src/gilded_rose');
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).to.equal("fixme");
   });
 });
